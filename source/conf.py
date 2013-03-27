ce7b2f9819960d33f03273fdbb804b1e5c382713
Ben Bangert <ben@groovie.org>
2013-03-27 12:55:42 -0700

    Update copyright.

--- source/conf.py
+++ source/conf.py
@@ -44,7 +44,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Heka Documentation'
-copyright = u'2012, Mozilla'
+copyright = u'2013, Mozilla'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
